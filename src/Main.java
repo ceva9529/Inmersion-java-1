6fabf7e5081e88afa9c46854e92de216ba08172d
Cesar Vazquez <asesor.ceva@gmail.com>
2024-05-03 01:29:03 -0600

    Inmersion JAVA primeros pasos

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos al mundo de JAVA!");
+        System.out.println("Pelicula: Extraño mundo del JACK");
+
+        int fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(evaluacion);
+        System.out.println(incluidoEnElPlanBasico);
+    }
+}
